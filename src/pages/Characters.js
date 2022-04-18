cf82621167ae535de25c7fe63b53fc0e9702281f
Denis <69271897+DelinFix@users.noreply.github.com>
2022-04-18 03:08:28 +0300

    Add comment 2

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,3 +1,4 @@
+//Страница всех персонажей, выводится через запрос, подаваемый при загрузке и при переходе на новую страницу
 import React,{useState, useEffect} from "react"
 import {Link} from "react-router-dom"
 
